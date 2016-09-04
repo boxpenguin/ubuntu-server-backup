54f142892741faeb2fe5d7859317fdb9d6cabd23
rakunko <jordan.uehara@gmail.com>
2016-09-04 18:46:56 -0400

    up

--- perform.sh
+++ perform.sh
@@ -62,21 +62,21 @@ perform_createdata () {
   perform_fill
   du -shBM /media/GRANOLA/*/ | sort -h >> /home/clara/Backups/1-Diskspace-$(date +\%F)
   Create weekly hard drive tests
-  # touch $FILE
-  # if [ -e $FILE ]
-  # then
-  #   echo "Running disk stats"
-  #   echo "Printing disk stats." >> $FILE
-  #   for i in a b c d e f; do
-  #     mount | grep sd$i | awk '{print $1, $2, $3}' >> $$FILE
-  #     /sbin/hdparm -Tt /dev/sd$i >> $FILE
-  #     echo "" >> $FILE
-  #   done
-  #   echo "END" >> $FILE
-  #   cat $FILE | /usr/bin/aha --title "Disk Stats" > $FILE.html
-  #   rm $FILE
-  # else
-  #   echo "File already exists."
+  touch $FILE
+  if [ -e $FILE ]
+  then
+    echo "Running disk stats"
+    echo "Printing disk stats." >> $FILE
+    for i in a b c d e f; do
+      mount | grep sd$i | awk '{print $1, $2, $3}' >> $$FILE
+      /sbin/hdparm -Tt /dev/sd$i >> $FILE
+      echo "" >> $FILE
+    done
+    echo "END" >> $FILE
+    cat $FILE | /usr/bin/aha --title "Disk Stats" > $FILE.html
+    rm $FILE
+  else
+    echo "File already exists."
   fi
 }
 
