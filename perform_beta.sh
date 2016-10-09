cac6ccc2a1bbedb301da87306fd4b49c036b1d07
Jordan Cameron Uehara <jordan.uehara@gmail.com>
2016-10-08 21:41:37 -0500

    ss
    ss

--- perform_beta.sh
+++ perform_beta.sh
@@ -2,6 +2,6 @@
 
 # Disk stats
 declare -a media_target=( '/media/DANISH/Media/TV\ Shows/' '/media/ECLAIR/Movies/' '/media/ECLAIR/Anime/' '/media/DANISH/Media/Special\ Libraries/' '/home/clara/' '/var/lib/plexmediaserver/' '/media/DANISH/Media/Music/' '/media/GRANOLA/Backups-Muffin/' '/media/' '/media/DANISH/' '/media/ECLAIR/' '/media/GRANOLA/' '/media/MOCHI/' )
-for i in ${#media_target[@]}); do
+for i in ${#media_target[@]}; do
   echo item: $i
 done
