d93e6845f64b85ad2fa055397ce8f0d1b39832f3
Jordan Cameron Uehara <jordan.uehara@gmail.com>
2016-10-08 23:08:14 -0500

    s
    ss

--- perform_beta.sh
+++ perform_beta.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Disk stats
-media_target=( '' '/media/DANISH/Media/TV\ Shows/' '/media/ECLAIR/Movies/' '/media/ECLAIR/Anime/' '/media/DANISH/Media/Special\ Libraries/' '/home/clara/' '/var/lib/plexmediaserver/' '/media/DANISH/Media/Music/' '/media/GRANOLA/Backups-Muffin/' '/media/' '/media/DANISH/' '/media/ECLAIR/' '/media/GRANOLA/' '/media/MOCHI/' )
-for i in ${#media_target[@]}; do
-  echo Total Amount: $media_target[i]
+media_target=( '/media/DANISH/Media/TV*' '/media/ECLAIR/Movies/' '/media/ECLAIR/Anime/' '/media/DANISH/Media/Special*' '/home/clara/' '/var/lib/plexmediaserver/' '/media/DANISH/Media/Music/' '/media/GRANOLA/Backups-Muffin/' '/media/' '/media/DANISH/' '/media/ECLAIR/' '/media/GRANOLA/' '/media/MOCHI/' )
+COUNTER=0
+while [ $COUNTER -lt ${#media_target[@]} ]; do
+#       echo "Counter: " $COUNTER
+#       echo "Entry: " ${media_target[$COUNTER]}
+        X=" MB "
+        du -shBM ${media_target[$COUNTER]} 2>/dev/null | awk -v var="$X" '{print substr($1,1, length($1)-1), var, $2, $3}'
+        let COUNTER=COUNTER+1
 done
