d0666de7bb2f974894a58b494d236d91ac618b57
Jordan Cameron Uehara <jordan.uehara@gmail.com>
2016-10-09 00:14:10 -0500

    sad
    asdsss

--- perform_beta.sh
+++ perform_beta.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
-# Variables
+# Global Variables
 DATE=$(date +\%F)
+
+# disk_stat variables
 DISKSTAT_FILE=/tmp/1-Diskspace-$DATE
-# Disk stats
+DISKSTAT_WEB=/var/www/html/_admin/storage.html
+touch $DISKSTAT_FILE  # Create diskstat file
 media_target=( '/media/DANISH/Media/TV*' '/media/ECLAIR/Movies/' '/media/ECLAIR/Anime/' '/media/DANISH/Media/Special*' '/home/clara/' '/var/lib/plexmediaserver/' '/media/DANISH/Media/Music/' '/media/GRANOLA/Backups-Muffin/' '/media/' '/media/DANISH/' '/media/ECLAIR/' '/media/GRANOLA/' '/media/MOCHI/' )
-COUNTER=0
-while [ $COUNTER -lt ${#media_target[@]} ]; do
-#       echo "Counter: " $COUNTER
-#       echo "Entry: " ${media_target[$COUNTER]}
-        X=" MB "
-        du -shBM ${media_target[$COUNTER]} 2>/dev/null | awk -v var="$X" '{print substr($1,1, length($1)-1), var, $2, $3}' >> $DISKSTAT_FILE
-        let COUNTER=COUNTER+1
-done
+
+# disk_parm Variables
+DISKPERF_FILE=/tmp/2-Diskperf-$DATE
+DISKPERF_WEB="/var/www/html/_admin/Diskstats/Diskstats-"$DATE
+touch $DISKPERF_WEB # Creat temp diskperf file
+
+disk_stat () {
+  COUNTER=0
+  while [ $COUNTER -lt ${#media_target[@]} ]; do
+    X=" MB "
+    du -shBM ${media_target[$COUNTER]} 2>/dev/null | awk -v var="$X" '{print substr($1,1, length($1)-1), var, $2, $3}' >> $DISKSTAT_FILE
+    let COUNTER=COUNTER+1
+  done
+  # Push to Web
+  cat $DISKSTAT_FILE | /usr/bin/aha --title "Disk Storage" > $DISKSTAT_WEB
+}
+
+disk_perf () {
+  echo "Printing disk stats." >> $DISKPERF_WEB
+  for i in $(mount | grep /dev/sd | awk '{print substr($1,1, length($1)-1)}' | uniq | sort); do
+    mount | grep sd$i | awk '{print $1, $2, $3}' >> $DISKPERF_WEB
+    /sbin/hdparm -Tt /dev/sd$i >> $DISKPERF_WEB
+    echo "" >> $DISKPERF_WEB
+  done
+  echo "END" >> $DISKPERF_WEB
+  cat $DISKPERF_WEB | /usr/bin/aha --title "Disk Performance" > $DISKPERF_WEB.html
+  rm $DISKPERF_WEB
+}
