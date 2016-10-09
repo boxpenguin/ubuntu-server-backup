c942afd09e3444002cec1a697e27ff8be6c5c916
Jordan Cameron Uehara <jordan.uehara@gmail.com>
2016-10-08 23:23:35 -0500

    xvdf
    sdfsdf

--- perform_beta.sh
+++ perform_beta.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# Variables
+DATE=$(date +\%F)
+DISKSTAT_FILE=/tmp/1-Diskspace-$DATE
 # Disk stats
 media_target=( '/media/DANISH/Media/TV*' '/media/ECLAIR/Movies/' '/media/ECLAIR/Anime/' '/media/DANISH/Media/Special*' '/home/clara/' '/var/lib/plexmediaserver/' '/media/DANISH/Media/Music/' '/media/GRANOLA/Backups-Muffin/' '/media/' '/media/DANISH/' '/media/ECLAIR/' '/media/GRANOLA/' '/media/MOCHI/' )
 COUNTER=0
@@ -7,6 +9,6 @@ while [ $COUNTER -lt ${#media_target[@]} ]; do
 #       echo "Counter: " $COUNTER
 #       echo "Entry: " ${media_target[$COUNTER]}
         X=" MB "
-        du -shBM ${media_target[$COUNTER]} 2>/dev/null | awk -v var="$X" '{print substr($1,1, length($1)-1), var, $2, $3}'
+        du -shBM ${media_target[$COUNTER]} 2>/dev/null | awk -v var="$X" '{print substr($1,1, length($1)-1), var, $2, $3}' >> $DISKSTAT_FILE
         let COUNTER=COUNTER+1
 done
