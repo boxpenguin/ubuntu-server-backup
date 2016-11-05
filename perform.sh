1e829c308db7615371f4abd552429f0dba3635cc
Jordan Cameron Uehara <jordan.uehara@gmail.com>
2016-11-05 17:55:20 -0400

    Version 0.9 Complete
    Updated to newer variables and functions to allow more generic usage.
    Needs to be documented better prior to final stable version 1.0

--- perform.sh
+++ perform.sh
@@ -1,141 +1,164 @@
 #!/bin/bash
-# Variables
+
+# User defined controls
+# Indicate where you would like your web(HTML) Disk storage stats kept (directory)
+DISK_STAT_DIR_WEB=/var/www/html/_admin/Disk_Storage/
+DISK_STAT_WEB_FILE_NAME="Disk_storage"
+# Indicate where you would like your web(HTML) Disk performance stats kept (directory)
+DISK_PERF_DIR_WEB=/var/www/html/_admin/Disk_Performance/
+DISK_PERF_WEB_FILE_NAME="Disk_performance"
+# Indicate where you would like to store your temp backup files (NCDU)
+BACKUP_DIR=/var/Backup/
+BACKUP_DIR_TEMP=/tmp/
+BACKUP_DEST_DIR=/media/GRANOLA/Backups-Muffin/Clara-tan.home/Clara-tan_core/
+BACKUP_SOURCE=( '/home/clara/' '/opt/' )
+# END USER defined
+
+# Global Variables
+DATE=$(date +\%F)
 OLDCONF=$(dpkg -l|grep "^rc"|awk '{print $2}')
 CURKERNEL=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
 LINUXPKG="linux-(image|headers|ubuntu-modules|restricted-modules)"
 METALINUXPKG="linux-(image|headers|restricted-modules)-(generic|i386|server|common|rt|xen)"
 OLDKERNELS=$(dpkg -l|awk '{print $2}'|grep -E $LINUXPKG |grep -vE $METALINUXPKG|grep -v $CURKERNEL)
-DATE=$(date +"%m-%d-%y")
-FILE="/var/www/html/_admin/Diskstats/Diskstats-"$DATE
 
-# Functions
-# crontab Controls
-perform_crontab () {
-  case $1 in
-    # Remove current root crontab
-    "remove") /usr/bin/crontab -r -u root ;;
-    # Print crontab
-    "print") /usr/bin/crontab -l -u root ;;
-    # Load default cron
-    "load") /usr/bin/crontab -u root /opt/ubuntu-server-backup/crontab.cron ;;
-  esac
+# disk_stat variables TODO setup and test for user defined controls
+DISKSTAT_FILE=$BACKUP_DIR/1-Diskspace-$DATE
+DISKSTAT_WEB=$DISK_STAT_DIR_WEB/$DISK_STAT_WEB_FILE_NAME-$DATE.html
+#echo "" > $DISKSTAT_FILE #remove me later
+media_target=( '/media/DANISH/Media/TV*' '/media/ECLAIR/Movies/' '/media/ECLAIR/Anime/' '/media/DANISH/Media/Special*' '/home/clara/' '/var/lib/plexmediaserver/' '/media/DANISH/Media/Music/' '/media/GRANOLA/Backups-Muffin/' '/media/' '/media/DANISH/' '/media/ECLAIR/' '/media/GRANOLA/' '/media/MOCHI/' )
+
+# disk_perf Variables
+DISKPERF_FILE=$BACKUP_DIR/2-Diskperf-$DATE
+DISKPERF_WEB=$DISK_PERF_DIR_WEB/$DISK_PERF_WEB_FILE_NAME-$DATE.html
+#echo "" > $DISKPERF_WEB #remove me later
+
+# backup Variables
+BACKUP_FILE=$(hostname)-$DATE.tar.gz
+BACKUP_FILE_SHA=$BACKUP_FILE.sha256
+BACKUP=$BACKUP_DIR_TEMP/$BACKUP_FILE
+BACKUP_SHA=$BACKUP_DIR_TEMP/$BACKUP_FILE_SHA
+#echo "" > $BACKUP #remove me later
+if [ -e $BACKUP_DIR ]; then
+  echo "Backup drive located: " $BACKUP_DIR
+else
+  echo "Unable to locate backup directory set as: "
+  mkdir $BACKUP_DIR # Creates Backup Directory
+fi
+
+# NCDU Variables
+NCDU_FILE=3-NCDU-$DATE
+
+# Pre-script work
+prework () {
+  /sbin/stop plexmediaserver
+  /usr/bin/crontab -r -u root
 }
 
-# plexserver Controls
-perform_plex () {
-  case $1 in
-    # start
-    "start") /sbin/start plexmediaserver ;;
-    # stop
-    "stop") /sbin/stop plexmediaserver ;;
-  esac
+# post-script work
+postwork () {
+  /sbin/start plexmediaserver
+  /usr/bin/crontab -u root /opt/ubuntu-server-backup/crontab.cron
 }
 
-# Delete previous backups
-perform_deletedata () {
-  rm -rf /home/clara/Backups/1-* > /dev/null 2>&1
-  rm -rf /home/clara/Backups/2-* > /dev/null 2>&1
-}
-
-# Fill with null data
-perform_fill () {
-  echo "" >> /home/clara/Backups/1-Diskspace-$(date +\%F)
-}
-# Create Disk information
-# TODO Could be for looped with an array containing the locations need to variable the location of 1-Diskspace
-perform_createdata () {
-  perform_deletedata
-  touch /home/clara/Backups/1-Diskspace-$(date +\%F)
-  df -m | grep -v none > /home/clara/Backups/1-Diskspace-$(date +\%F)
-  perform_fill
-  du -shBM /*/ | sort -h >> /home/clara/Backups/1-Diskspace-$(date +\%F)
-  perform_fill
-  du -shBM /home/*/ | sort -h >> /home/clara/Backups/1-Diskspace-$(date +\%F)
-  perform_fill
-  du -shBM /var/lib/*/ | sort -h >> /home/clara/Backups/1-Diskspace-$(date +\%F)
-  perform_fill
-  du -shBM /media/DANISH/Media/*/ | sort -h >> /home/clara/Backups/1-Diskspace-$(date +\%F)
-  perform_fill
-  du -shBM /media/ECLAIR/*/ | sort -h >> /home/clara/Backups/1-Diskspace-$(date +\%F)
-  perform_fill
-  du -shBM /media/MOCHI/*/ | sort -h >> /home/clara/Backups/1-Diskspace-$(date +\%F)
-  perform_fill
-  du -shBM /media/GRANOLA/*/ | sort -h >> /home/clara/Backups/1-Diskspace-$(date +\%F)
-  # Create weekly hard drive tests
-  touch $FILE
-  if [ -e $FILE ]
-  then
-    echo "Running disk stats"
-    echo "Printing disk stats." >> $FILE
-    for i in a b c d e f; do
-      mount | grep sd$i | awk '{print $1, $2, $3}' >> $$FILE
-      /sbin/hdparm -Tt /dev/sd$i >> $FILE
-      echo "" >> $FILE
-    done
-    echo "END" >> $FILE
-    cat $FILE | /usr/bin/aha --title "Disk Stats" > $FILE.html
-    rm $FILE
+# Mr Worfs plex updater too required
+plex_upgrade () {
+  if [ -e /opt/plexupdate/plexupdate.sh ]; then
+    /opt/plexupdate/plexupdate.sh -u -a
   else
-    echo "File already exists."
+    echo "No upgrade script found."
   fi
 }
-
-# NCDU Controls
-perform_ncdu () {
-  /usr/bin/ncdu / -x -o /home/clara/Backups/2-NCDU-$(date +\%F)
+# Disk Storage Stats
+disk_stat () {
+  echo "Starting Disk stats" #debugging
+  COUNTER=0
+  while [ $COUNTER -lt ${#media_target[@]} ]; do
+    X=" MB "
+    du -shBM ${media_target[$COUNTER]} 2>/dev/null | awk -v var="$X" '{print substr($1,1, length($1)-1), var, $2, $3}' >> $DISKSTAT_FILE
+    let COUNTER=COUNTER+1
+  done
+  # Push to Web
+  cat $DISKSTAT_FILE | /usr/bin/aha --title "Disk Storage" > $DISKSTAT_WEB
 }
 
-# Backup Script
-perform_backup () {
-  tar cfh - /home/clara/Backups/ /home/clara/tools /opt/ | pigz --best > "/media/GRANOLA/Backups-Muffin/Clara-tan.home/Clara-tan_core/Clara-tan_core-$(date +\%F).tar.gz"
-  /bin/ls -ash "/media/GRANOLA/Backups-Muffin/Clara-tan.home/Clara-tan_core/Clara-tan_core-$(date +\%F).tar.gz"
-  /usr/bin/sha256sum "/media/GRANOLA/Backups-Muffin/Clara-tan.home/Clara-tan_core/Clara-tan_core-$(date +\%F).tar.gz" > "/media/GRANOLA/Backups-Muffin/Clara-tan.home/Clara-tan_core/Clara-tan_core-$(date +\%F).tar.gz.sha256"
-  /usr/bin/sha256sum -c "/media/GRANOLA/Backups-Muffin/Clara-tan.home/Clara-tan_core/Clara-tan_core-$(date +\%F).tar.gz.sha256"
+# Disk Performance stats
+disk_perf () {
+  echo "Starting performance stats" #debugging
+  echo "Printing disk stats." >> $DISKPERF_FILE
+  for i in $(mount | grep /dev/sd | awk '{print substr($1,1, length($1)-1)}' | uniq | sort); do
+    mount | grep $i | awk '{print $1, $2, $3}' >> $DISKPERF_FILE
+    /sbin/hdparm -Tt $i >> $DISKPERF_FILE
+    echo "" >> $DISKPERF_FILE
+  done
+  echo "END" >> $DISKPERF_FILE
+  cat $DISKPERF_FILE | /usr/bin/aha --title "Disk Performance" > $DISKPERF_WEB
+}
+
+# NCDU export to backup directories
+ncdu () {
+  echo "Starting ncdu" #debugging
+  /usr/bin/ncdu / -x -o $BACKUP_DIR/$NCDU_FILE
+}
+
+# Backup Func
+backup () {
+  echo "Starting backups" #debugging
+  tar cfh - $BACKUP_DIR ${BACKUP_SOURCE[@]} | pigz --best > $BACKUP
+  /bin/ls -ash $BACKUP
+  /usr/bin/sha256sum $BACKUP > $BACKUP_SHA
+  /usr/bin/sha256sum -c $BACKUP_SHA
+  rsync -avp --progress $BACKUP $BACKUP_SHA $BACKUP_DEST_DIR
+  rm -rf $BACKUP $BACKUP_SHA $BACKUP_DIR/* # Delete all temp backup files
 }
 
 # apt-get upgrade
-perform_apt_getup () {
-  service pgl stop
-  sudo /usr/bin/apt-get update > /dev/null 2>&1
-  sudo /usr/bin/apt-get -yq upgrade
-  service pgl start
+apt_get_up () {
+  echo "Starting apt-get updates" #debugging
+  /usr/bin/apt-get update > /dev/null 2>&1
+  /usr/bin/apt-get -yq upgrade
 }
 
 # apt-get clean up
-perform_apt_getclean () {
-  sudo aptitude clean
-  sudo aptitude -y purge $OLDCONF
-  sudo aptitude -y purge $OLDKERNELS
+apt_get_clean () {
+  echo "Starting apt-get cleanup" #debugging
+  aptitude clean
+  aptitude -y purge $OLDCONF
+  aptitude -y purge $OLDKERNELS
 }
 
 case $1 in
   "--full")
-  perform_crontab remove
-  perform_plex stop
-  perform_createdata
-  perform_ncdu
-  perform_backup
-  perform_plex start
-  perform_apt_getup
-  perform_apt_getclean
-  perform_crontab load
+  prework
+  disk_stat
+  disk_perf
+  ncdu
+  backup
+  apt_get_up
+  apt_get_clean
+  postwork
   ;;
   "--backup")
-  perform_createdata
-  perform_ncdu
-  perform_backup
+  prework
+  disk_stat
+  disk_perf
+  ncdu
+  backup
+  postwork
   ;;
   "--apt-get-up")
-  perform_apt_getup
-  perform_apt_getclean
+  prework
+  apt_get_up
+  apt_get_clean
+  postwork
   ;;
   "--test")
-  perform_createdata
+  backup
   ;;
   "")
   echo "--full; performs full backup and ubuntu updates"
   echo "--backup; only performs update, clears previous data"
   echo "--apt-get-up; performs apt-get updates and cleans up"
-  echo "--test; whatever Jordan decides"
+  echo "--test; whatever user decides"
   ;;
 esac
